6d828b83ece4aa4b5072f7becf37d53dcf3386b6
Hai Pham <haipham23@gmail.com>
2017-05-02 18:48:56 +0800

    fix eslint for node, add precommit hook

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(helmet({
-  noCache: true
+  noCache: true,
 }));
 app.use(compression());
 app.get('/version', require('version-healthcheck'));
-app.get('/service-worker.js', function (req, res) {
+app.get('/service-worker.js', (req, res) => {
   res.sendFile(path.join(__dirname, '/service-worker.js'));
 });
 app.use(express.static(__dirname + '/build/default'));
 
-app.listen(port, function (err) {
+app.listen(port, (err) => {
   if (err) {
     logger.info(err);
   } else {
